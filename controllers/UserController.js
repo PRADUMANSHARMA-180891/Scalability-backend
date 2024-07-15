1e01633a3db7605291fde5490cc1dcc0c90c33ff
praduman.cuh@gmail.com <praduman.cuh@gmail.com>
2024-07-15 17:45:11 +0530

    add user fields

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,39 +1,75 @@
 const User = require('../models/UserModels');
 
-//Login for user
+// Login for user
+const UserLogin = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      username,
+      user_photo,
+      phone_number,
+      p_isd,
+      whatsapp_no,
+      w_isd,
+      otp,
+      position,
+      is_login,
+      is_verified,
+      company_id,
+      departments_id,
+      status,
+      remember_token
+    } = req.body;
 
-const UserLogin = async(req,res)=>{
-     try {
-        const {email, password} = req.body;
-        if(!email,!password){
-            res.status(404).json({message : "email and password required"});
-        }
-        const postLoginData = await User.create({
-            email,
-            password
-        });
-        if(!postLoginData){
-            res.status(404).json({message: "something went worng while creating user"})
-        }
-
-        res.status(200).json(postLoginData);
-     } catch (error) {
-        res.status(400).json({message: "something wrong while posting data"});
-     }
-}
-
-// get user data
-
-const GetUserData = async(req,res)=>{
-    try {
-        const getUser = await User.findAll({});
-        if(!getUser){
-            res.status(404).json({message:"something went wrong while getting data"})
-        }
-        res.status(201).json(getUser);
-    } catch (error) {
-        res.status(400).json(error, {message: "something went wrong while getting user data"})
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
     }
-}
+
+    const postLoginData = await User.create({
+      name,
+      email,
+      password,
+      username,
+      user_photo,
+      phone_number,
+      p_isd,
+      whatsapp_no,
+      w_isd,
+      otp,
+      position,
+      is_login,
+      is_verified,
+      company_id,
+      departments_id,
+      status,
+      remember_token
+    });
+
+    if (!postLoginData) {
+      return res.status(500).json({ message: "Something went wrong while creating user" });
+    }
+
+    res.status(201).json(postLoginData);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong while posting data", error });
+  }
+};
+
+// Get user data
+const GetUserData = async (req, res) => {
+  try {
+    const getUser = await User.findAll({
+        attributes: ['name','email',"password","username","user_photo","phone_number"]
+    });
+    if (!getUser) {
+      return res.status(404).json({ message: "No user data found" });
+    }
+    res.status(200).json(getUser);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong while getting user data", error });
+  }
+};
 
 module.exports = { UserLogin, GetUserData };
--- models/UserModels.js
+++ models/UserModels.js
@@ -2,8 +2,68 @@ const sequelize = require('../Config/DBs');
 const Datatypes = require('sequelize');
 
 const User = sequelize.define('user',{
+    name: {
+        type: Datatypes.STRING,
+        allowNull: true,
+    },
     email: {type:Datatypes.STRING, allowNull: false },
-    password: {type: Datatypes.STRING, allowNull: false}
+    password: {type: Datatypes.STRING, allowNull: false},
+    username: {
+        type: Datatypes.STRING,
+        allowNull: true,
+    },
+    user_photo: {
+        type: Datatypes.STRING,
+        allowNull: true,
+    },
+    phone_number: {
+        type: Datatypes.INTEGER,
+        allowNull: true,
+    },
+    p_isd: {
+        type: Datatypes.STRING,
+        allowNull: true,
+    },
+    whatsapp_no: {
+        type: Datatypes.STRING,
+        allowNull: true,
+    },
+    w_isd: {
+        type: Datatypes.STRING,
+        allowNull: true,
+    },
+    otp: {
+        type: Datatypes.STRING,
+        allowNull: true,
+    },
+    position: {
+        type: Datatypes.STRING,
+        allowNull: true,
+    },
+    is_login: {
+        type: Datatypes.BOOLEAN,
+        defaultValue: 0,
+    },
+    is_verified: {
+        type: Datatypes.BOOLEAN,
+        defaultValue: 0,
+    },
+    company_id: {
+        type: Datatypes.INTEGER,
+        allowNull: true,
+    },
+    departments_id: {
+        type: Datatypes.INTEGER,
+        allowNull: true,
+    },
+    status: {
+        type: Datatypes.BOOLEAN,
+        defaultValue: 0,
+    },
+    remember_token: {
+        type: Datatypes.TEXT,
+        allowNull: true,
+    }
 });
 
 module.exports = User;
