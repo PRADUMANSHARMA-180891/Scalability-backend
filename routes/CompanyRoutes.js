925d8754350fac110b3eb39e8e8be1a3c0887d97
PRADUMANSHARMA-180891 <praduman.cuh@gmail.com>
2024-07-20 10:13:10 +0530

    add route name for company

--- routes/CompanyRoutes.js
+++ routes/CompanyRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { CreateCompany,getCompanyData, deleteCompany } = require('../controllers/CompanyControllers');
 
-
+// company routes
 router.post('/create',CreateCompany);
 router.get("/getcompany", getCompanyData);
 router.delete('/deletecompany/:id',deleteCompany);
