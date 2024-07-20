2fed465c16af95495484733c44d2de501bc6e58d
PRADUMANSHARMA-180891 <praduman.cuh@gmail.com>
2024-07-20 15:56:17 +0530

    compeleted search by name functionality

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { UserLogin,GetUserData, updateUser, upload, SearchUsersByName } = require("../controllers/UserController");
+const { UserLogin,GetUserData, updateUser, upload, SearchUsersByName, getUserById } = require("../controllers/UserController");
 const VerifyToken = require('../middleware/AuthMiddleware');
 
 //
@@ -9,5 +9,5 @@ router.post('/postlogin', UserLogin);
 router.get('/getlogin',GetUserData);
 router.put('/updateuser/:Id',upload.single('user_photo'), updateUser);
 router.get('/search',SearchUsersByName);
-router.get('/search/:id',SearchUsersByName);
+router.get('/search/:id',getUserById);
 module.exports = router
