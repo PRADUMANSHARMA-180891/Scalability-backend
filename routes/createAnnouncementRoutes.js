22b2a47eed770d98ab9abcbbfe779584e7b069cb
praduman.cuh@gmail.com <praduman.cuh@gmail.com>
2024-10-18 18:38:22 +0530

    added delete Announcement

--- routes/createAnnouncementRoutes.js
+++ routes/createAnnouncementRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {createAnnouncement, getAnnouncement, editAnnouncement} = require("../controllers/createAnnouncementController");
+const {createAnnouncement, getAnnouncement, editAnnouncement, deleteAnnouncement} = require("../controllers/createAnnouncementController");
 
 
 router.post('/create', createAnnouncement);
 router.get('/get/:id', getAnnouncement);
 router.put('/update/:id', editAnnouncement);
-router.delete('/delete/:id', editAnnouncement);
+router.delete('/delete/:id', deleteAnnouncement);
 
 module.exports = router;
